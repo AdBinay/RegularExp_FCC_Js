9e3c7a0177c30be54b4526bd930629c591fb40fa
Binay77 <binayadh77@gmail.com>
2023-12-03 12:30:51 +0545

    FCC-REGEx-jS

---
+++ 022_Restrict Possible Usernames.js
@@ -0,0 +1,6 @@
+// Restrict Possible Usernames
+
+let username = "JackOfAllTrades";
+let userCheck = /^[a-z][a-z]+\d*$|^[a-z]\d\d+$/i;
+let result = userCheck.test(username);
+console.log(result)
---
+++ 032_Remove Whitespace from Start and End.js
@@ -0,0 +1,12 @@
+// Remove Whitespace from Start and End
+
+/*Sometimes whitespace characters around strings are not wanted but are there. Typical processing of strings is to remove the whitespace at the start and end of it.
+
+Write a regex and use the appropriate string methods to remove whitespace at the beginning and end of strings.
+
+Note: The String.prototype.trim() method would work here, but you'll need to complete this challenge using regular expressions.*/
+
+
+let hello = "   Hello, World!  ";
+let wsRegex = /^\s+|\s+$/g; // Change this line
+let result = hello.replace(wsRegex, ""); // Change this line
